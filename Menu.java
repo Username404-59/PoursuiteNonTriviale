d70b72646d85d8eb2a8aa05326c904360ada3769
aoi2409 <ombrette.baulard@gmail.com>
2025-12-10 10:08:25 +0100

    Update Menu.java

--- Menu.java
+++ Menu.java
@@ -1,53 +1,55 @@
+import extensions.File;
 class Menu extends Program{
-    void aideMenu() {
-        println("1.Cases\n2.Combat\n3.Bonus");
+
+//-----fonctions aide-----
+
+    void lireFichier(String cheminFichier){
+        File fichier = newFile(cheminFichier);
+        while (ready(fichier)) {
+            println(readLine(fichier));
+        }
+        String saisie = " ";
+        do{
+            saisie=readString();
+        }while(!equals(saisie,""));
+        aide();
     }
 
-    void aideCases() {
-        println("Dans ce jeu, il existe 4 types de cases.\nLe premier type est bien sur la case vide. Son numéro est inscrit à l'intérieur.\nEnsuite, nous avons la case où se trouve le joueur. Elle est marquée par un 'X'.\n");
-    }
-
-    void aide() {
-        aideMenu();
+    void aide(){
+        println("1.Cases\n2.Combat\n3.Bonus\n4.retour");
         int saisie;
         do{
-            print("Veuillez choisir un nombre entre 1 et 3 : ");
+            print("Veuillez choisir un nombre entre 1 et 4 : ");
             saisie = readInt();
-        } while(saisie<1 || saisie>3);
-        if (saisie==1) {
-            aideCases();
+        }while(saisie<1 || saisie>4);
+        if(saisie==1){
+            lireFichier("aide/aide cases");
+        }else if(saisie==2){
+            lireFichier("aide/aide combat");
+        }else if(saisie==3){
+            lireFichier("aide/aide bonus");
+        }else{
+            afficherMenu();
         }
     }
 
-    void afficherMenu() {
-        println("1.Jouer\n2.Aide\n3.Option");
-    }
+//-----fonctions Menu-----
 
-    void algorithm() {
-        afficherMenu();
+    void afficherMenu(){
+        println("1.Jouer\n2.Aide\n3.Option");
         int saisie;
         do{
             print("Veuillez choisir un nombre entre 1 et 3 : ");
             saisie = readInt();
-        } while (saisie<1 || saisie>3);
-        if (saisie==2) {
+        }while(saisie<1 || saisie>3);
+        if (saisie==2){
             aide();
         }
     }
-}
 
-class Plateau {
-    void remplacer(String plateau, int _case, String _contenu) {
-        // TODO Boucle sur plateau; quand on trouve un cas où plateau[i] + plateau[i+1] == _case (ou == "0"+_case si (_case <= 9)), on s'arrête et on garde l'index
-        // Ensuite on utilisera l'index pour remplacer avec _contenu
-    }
-}
+//-----algo principal-----
 
-class Jeu {
-    boolean fini = false;
-    void jouer() {
-        while (!fini) {
-
-        }
+    void algorithm(){
+        afficherMenu();
     }
 }
