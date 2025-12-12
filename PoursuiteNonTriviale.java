902382ca0199042845560044df15e9fa44b8a7b0
Username404-59 <w.iron.zombie@gmail.com>
2025-12-12 15:33:07 +0100

    Affichage du titre du jeu
    Signed-off-by: Username404-59 <w.iron.zombie@gmail.com>

--- PoursuiteNonTriviale.java
+++ PoursuiteNonTriviale.java
@@ -76,6 +76,7 @@ class PoursuiteNonTriviale extends Program {
     void afficherMenu() {
         int saisie;
         do {
+            println(fichierTexte("ascii/PoursuiteNonTriviale.txt"));
             println("1.Jouer\n2.Aide\n3.Option\n4.Quitter");
             saisie = choixNombre(1, 4);
             if (saisie == 1) jouer();
