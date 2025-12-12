c45b4ed991b24996e76719321ff4bbfa23260b5f
Username404-59 <w.iron.zombie@gmail.com>
2025-12-12 03:40:18 +0100

    cases(), une fonction pour plus tard
    Pas testée ou quoi que ce soit, elle servira dans remplacer et ptet autre chose
    
    Signed-off-by: Username404-59 <w.iron.zombie@gmail.com>

--- PoursuiteNonTriviale.java
+++ PoursuiteNonTriviale.java
@@ -52,8 +52,32 @@ class PoursuiteNonTriviale extends Program {
     void algorithm() { afficherMenu(); }
 
 //-----plateau-----
+    final int NB_CASES = 20;
+    int[][] cases(String plateau) { // Récupère indices + valeurs des nombres dans le string de plateau; fonction deux-en-un :)
+        int[][] resultat = new int[NB_CASES][2];
+        int cptCases = 0;
+        for (int i = 0; i < length(plateau) - 1; i++) {
+            final int actuel = charAt(plateau, i) - '0';
+            final int prochain = charAt(plateau, i+1) - '0';
+            final boolean nb_un_chiffre = actuel == 0;
+            final boolean nb_deux_chiffres = actuel >= 0 && actuel <= 9 && prochain >= 0 && prochain <= 9;
+
+            if (nb_un_chiffre || nb_deux_chiffres) {
+                resultat[cptCases] = new int[]{i, -1}; // [0] = index, [1] = valeur (ici -1 en attendant les conditions juste en dessous)
+                cptCases++; i++;
+            }
+            if (nb_un_chiffre) {
+                resultat[cptCases][1] = prochain;
+            } else if (nb_deux_chiffres) {
+                resultat[cptCases][1] = (actuel * 10) + prochain;
+            }
+        }
+        return resultat;
+    }
+
     void remplacer(String plateau, int _case, String _contenu) {
         // TODO Boucle sur plateau; quand on trouve un cas où plateau[i] + plateau[i+1] == _case (ou == "0"+_case si (_case <= 9)), on s'arrête et on garde l'index
         // Ensuite on utilisera l'index pour remplacer avec _contenu
+        // EDIT: On peut utiliser la fonction cases() que j'ai faite au dessus
     }
 }
